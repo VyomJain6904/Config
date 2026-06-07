b7f58ecb001b713c4335b40d08c1751fe0753337
VyomJain6904 <vyom.malo6904@gmail.com>
2026-06-07 14:17:23 +0530

    Gestures Function Added

--- hypr/modules/input.lua
+++ hypr/modules/input.lua
@@ -25,3 +25,51 @@ hl.gesture({
     direction = "horizontal",
     action = "workspace"
 })
+
+hl.gesture({
+    fingers = 4,
+    direction = "left",
+    action = function()
+        hl.dispatch(hl.dsp.focus({ direction = "left" }))
+    end
+})
+
+hl.gesture({
+    fingers = 4,
+    direction = "right",
+    action = function()
+        hl.dispatch(hl.dsp.focus({ direction = "right" }))
+    end
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "up",
+    action = function()
+        hl.dispatch(hl.dsp.exec_cmd("wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 10%+"))
+    end
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "down",
+    action = function()
+        hl.dispatch(hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-"))
+    end
+})
+
+hl.gesture({
+    fingers = 4,
+    direction = "up",
+    action = function()
+        hl.dispatch(hl.dsp.exec_cmd("brightnessctl set 10%+"))
+    end
+})
+
+hl.gesture({
+    fingers = 4,
+    direction = "down",
+    action = function()
+        hl.dispatch(hl.dsp.exec_cmd("brightnessctl set 10%-"))
+    end
+})

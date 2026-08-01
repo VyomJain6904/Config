8e351e2aa0de0d7a42fba53abd5c105bfec6d833
VyomJain6904 <vyom.malo6904@gmail.com>
2026-08-02 00:30:40 +0530

    feat(nvim): pin telescope to tag 0.1.8

--- nvim/lua/plugins/editor/telescope.lua
+++ nvim/lua/plugins/editor/telescope.lua
@@ -25,7 +25,6 @@ end
 
 return {{
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
     dependencies = {'nvim-lua/plenary.nvim', 'echasnovski/mini.icons', {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make'
